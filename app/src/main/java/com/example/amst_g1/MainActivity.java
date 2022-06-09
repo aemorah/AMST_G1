5f98d97e603361698ec55c917c3af740936565eb
jaimemoransantos <jaimemoransantos@gmail.com>
2022-06-09 00:47:43 -0500

    Cambios y adición del Activity formulario_registro

--- app/src/main/java/com/example/amst_g1/MainActivity.java
+++ app/src/main/java/com/example/amst_g1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.amst_g1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -25,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
         txtPassword = (EditText) findViewById(R.id.txtPassword);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnRegistro = (Button) findViewById(R.id.btnRegistro);
+    }
+
+    public void registrarse(View view) {
+        Intent intent = new Intent(this, formulario_registro.class);startActivity(intent);
+    }
+    public void login(View view) {
+        Toast toast=Toast.makeText(getApplicationContext(),"Usted no cuenta con un usuario",Toast.LENGTH_SHORT); toast.show();
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnLogin){
+            Log.d("mensaje","ïngreso");
+        }else if(v.getId() == R.id.btnRegistro) {
+        }
     }
 
 }
