e2d82501a793ff425b381648dec56b1558ae8bff
aemorah <aleemora@espol.edu.ec>
2022-06-09 20:54:41 -0500

    ajustes de errores

--- app/src/main/java/com/example/amst_g1/google_maps.java
+++ app/src/main/java/com/example/amst_g1/google_maps.java
@@ -1,48 +1,14 @@
 package com.example.amst_g1;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class google_maps extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
+public class google_maps extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapView);
-        mapFragment.getMapAsync(this);
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
-    public void onMapReady(GoogleMap googleMap) {
-        Bundle bundle = getIntent().getExtras();
-        mMap = googleMap;
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-
-        LatLng marcador = new LatLng(bundle.getDouble("lat"), bundle.getDouble("lng"));
-        mMap.addMarker(new MarkerOptions().position(marcador).title(bundle.getString("titulo")));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marcador,10));
+        setContentView(R.layout.activity_grafico_lineal);
     }
 
 }
