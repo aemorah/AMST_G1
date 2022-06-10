3f7e2aaa769ea92d66317382a3528322ac7f061f
aemorah <aleemora@espol.edu.ec>
2022-06-09 22:01:18 -0500

    Se realizo los graficos de linea

--- app/src/main/java/com/example/amst_g1/calendar_view.java
+++ app/src/main/java/com/example/amst_g1/calendar_view.java
@@ -1,7 +1,9 @@
 package com.example.amst_g1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class calendar_view extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class calendar_view extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_view);
+    }
+
+    public void regresar(View view) {
+        Intent intent = new Intent(this, menu_ingreso.class);
+        startActivity(intent);
     }
 }
