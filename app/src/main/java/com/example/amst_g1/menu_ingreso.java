e68c721aa290af4e8abc8eaf0d8c0bccb9b86f73
aemorah <aleemora@espol.edu.ec>
2022-06-09 20:18:09 -0500

    Ajuste en los botones y pantallas

--- app/src/main/java/com/example/amst_g1/menu_ingreso.java
+++ app/src/main/java/com/example/amst_g1/menu_ingreso.java
@@ -4,19 +4,49 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
 import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 public class menu_ingreso extends AppCompatActivity {
-
-    FloatingActionsMenu menuBotones;
-    FloatingActionButton botCalen,botYou,botMapas,botGra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_ingreso);
+
+        //FloatingActionButton menuBotones = findViewById(R.id.openMenu);
+        FloatingActionButton botCalen = (FloatingActionButton) findViewById(R.id.idcalendario);
+        FloatingActionButton botGra = (FloatingActionButton) findViewById(R.id.idgrafico);
+        FloatingActionButton botYou = (FloatingActionButton) findViewById(R.id.idyoutube);
+        FloatingActionButton botMapas = (FloatingActionButton) findViewById(R.id.idmapas);
+
+
+        botMapas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                google_maps(view);
+            }
+        });
+
+        botYou.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vid(view);
+            }
+        });
+
+        botCalen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calen(view);
+            }
+        });
+
+        botGra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grafic_lineal(view);
+            }
+        });
     }
 
     public void google_maps(View view) {
